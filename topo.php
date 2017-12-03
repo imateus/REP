4bc5a341e750944d10501615270a31f796fc2f27
Gui1andre <gui1andre89@gmail.com>
2017-12-03 02:24:51 -0200

    submenu

--- topo.php
+++ topo.php
@@ -5,7 +5,7 @@ ul {
     padding: 0;
     overflow: hidden;
     background-color: #9a9797;
-    height: 55px;
+    height: auto;
 }
 
 li {
@@ -21,13 +21,60 @@ li a {
     padding: 10px;    
  }
 
-.active {
+  
+/* Style The Dropdown Button */
+.dropbtn {
     background-color: #1498d5;
-    height: 55px;   
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+    position: relative;
+    display: inline-block;
+    float: right;
+    
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #1498d5;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index:10;
+
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {
+    background-color: #1498d5;
 }
 
 </style>
 <nav class="navbar navbar-default navbar-fixed-top">
+    
         <ul>
             <li><img src="imagens/Logo.png" style="margin-top: 13px;margin-left: 10px;"></li>
             <li><a>
@@ -38,7 +85,19 @@ li a {
                 </button>
                 </a>
             </li>
-            <li style="float:right"><a class="active" href="#about"><img style="border-radius: 50%;" id="profile-img" 
-            class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"><?php echo " ".$usuario;?></li>
-        </ul>
+       
+            <div class="dropdown">
+  <button class="dropbtn"><img style="border-radius: 50%;" id="profile-img" 
+            class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"><?php echo " ".$usuario;?></button>
+  <div class="dropdown-content">
+    <a href="logout.php">Sair</a>
+    
+  </div>
+</div>
+
+            
+        
+
+</div>
+    
 </nav>
